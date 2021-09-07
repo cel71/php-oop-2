cad326b177c52d526e8144ca8db3b47e69647e44
cel71 <celestialc71@libero.it>
2021-09-07 17:46:14 +0200

    php-oop-2 es2

--- e-shop.php
+++ e-shop.php
@@ -47,25 +47,28 @@ class User {
     protected $email;
     protected $number;
     protected $city;
+    protected $creditCard;
 
-    public function __construct($name, $surname, $age, $email, $number, $city){
+    public function __construct($name, $surname, $age, $email, $number, $city, $creditCard){
         $this->name = $name;
         $this->surname = $surname;
         $this->age = $age;
         $this->email = $email;
         $this->number = $number;
         $this->city = $city;
+        $this->creditCard = $creditCard;
     }   
 }
 
 class PremiumUser {
-    public function __construct($name, $surname, $age, $email, $number, $city){
+    public function __construct($name, $surname, $age, $email, $number, $city, $creditCard){
         $this->name = $name;
         $this->surname = $surname;
         $this->age = $age;
         $this->email = $email;
         $this->number = $number;
         $this->city = $city;
+        $this->creditCard = $creditCard;
     }   
 }
 
@@ -80,5 +83,18 @@ $eshop->addProducts($secondo);
 $eshop->addProducts($terzo);
 
 var_dump($eshop->getProducts());
+
+$nuovoUtente1 = new User('Mario', 'Rossi', 45, 'aaa.gmail.com', 345123, 'Roma', 6416841651);
+$nuovoUtente2 = new User('Lara', 'Neri', 40, 'bbb.gmail.com', 789456, 'Torino', 49484684684);
+
+
+var_dump($nuovoUtente1);
+var_dump($nuovoUtente2);
+
+$utentePremium1 = new PremiumUser('Franco', 'Bianchi', 75, 'ccc.gmail.com', 1111111, 'Milano', 46988555);
+$utentePremium2 = new PremiumUser('Maria', 'Gialli', 44, 'ddd.gmail.com', 2222222, 'Palermo', 5456468464);
+
+var_dump($utentePremium1);
+var_dump($utentePremium2);
 
 ?>
